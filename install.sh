d96ceccdf89e0d9702817f058cbb139e245c7e76
limyc <limyc.sp@gmail.com>
2022-06-15 23:53:55 +0000

    change cp options

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ ln -s -f ~/.dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/.dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ln -s -f ~/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
 
-cp -u ~/.dotfiles/.zshenv ~/.zshenv
-cp -u ~/.dotfiles/.gitconfig ~/.gitconfig
+cp -n ~/.dotfiles/.zshenv ~/.zshenv
+cp -n ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo 'Success.\n'
 echo 'Customize the files below.'
